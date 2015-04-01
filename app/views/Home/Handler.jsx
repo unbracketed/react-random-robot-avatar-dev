c06f656afc16f398c8cb6e305bfe8df0a6bba5f8
Brian Luft <brian@lincolnloop.com>
2015-04-01 08:19:08 -0700

    Moves part construction into the avatar component

--- app/views/Home/Handler.jsx
+++ app/views/Home/Handler.jsx
@@ -1,6 +1,6 @@
 var React = require('react')
 var {Link} = require('react-router')
-var {RandomBotAvatar, Rectangle, Eyes, Head, Ears, Top, Collar} = require('react-random-bot-avatar')
+var RandomBotAvatar = require('react-random-bot-avatar')
 
 
 var MainHandler = React.createClass({
@@ -8,28 +8,10 @@ var MainHandler = React.createClass({
     return (
       <div>
 				<Link to="another-view">Another View</Link>
-        <div>Main View</div>
-        <RandomBotAvatar height="600" width="600">
-          <Top/>
-          <Head/>
-          <Eyes/>
-          <Ears/>
-          <Collar/>
-        </RandomBotAvatar>
-        <RandomBotAvatar height="300" width="200">
-          <Top/>
-          <Head/>
-          <Eyes/>
-          <Ears/>
-          <Collar/>
-        </RandomBotAvatar>
-        <RandomBotAvatar height="500" width="300">
-          <Top/>
-          <Head/>
-          <Eyes/>
-          <Ears/>
-          <Collar/>
-        </RandomBotAvatar>
+        <div>Main Viewz   gg</div>
+        <RandomBotAvatar height="600" width="600"/>
+        <RandomBotAvatar height="300" width="200"/>
+        <RandomBotAvatar height="500" width="300"/>
       </div>
     )
   }
